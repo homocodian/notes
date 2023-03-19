69d996fb3fb71bfc1cec0e9146d999885e4833e2
Ashish <ashishcbscboard@gmail.com>
2023-03-19 10:30:48 +0530

    refactor: removed unnecessary code which was changing body color in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
-import { useLayoutEffect, useState, Fragment } from "react";
+import { useState, Fragment } from "react";
 
 import Box from "@mui/system/Box";
-import { useTheme } from "@mui/material/styles";
 
 import Notes from "../components/main/Notes";
 import Profile from "../components/Profile";
@@ -12,46 +11,37 @@ import AddNoteModal from "../components/main/AddNoteModal";
 import { useAccountMenu } from "../context/AccountMenuContext";
 
 function Home() {
-  const { palette } = useTheme();
-  const accountMenuOptions = useAccountMenu();
-  const [isNoteModalOpen, setIsNoteModalOpen] = useState(false);
+	const accountMenuOptions = useAccountMenu();
+	const [isNoteModalOpen, setIsNoteModalOpen] = useState(false);
 
-  useLayoutEffect(() => {
-    if (palette.mode === "light") {
-      document.body.style.backgroundColor = "#f5f5f5";
-    } else {
-      document.body.style.backgroundColor = "#18181b";
-    }
-  }, [palette]);
-
-  return (
-    <Fragment>
-      <Box
-        sx={{
-          display: "flex",
-          width: "100%",
-          overflow: "auto",
-          paddingBottom: "15px",
-        }}
-      >
-        <SideDrawer />
-        <Box sx={{ width: "100%" }} mt={3}>
-          <Notes />
-        </Box>
-        <Profile
-          isOpen={accountMenuOptions.isProfileOpen}
-          setIsOpen={accountMenuOptions.setIsProfileOpen}
-        />
-        <AddNoteModal open={isNoteModalOpen} setOpen={setIsNoteModalOpen} />
-        <AlertMessage
-          open={accountMenuOptions.isError}
-          setOpen={accountMenuOptions.setIsError}
-          message="Logout error, please try later."
-        />
-      </Box>
-      <AddButton openAddTodoModal={setIsNoteModalOpen} />
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Box
+				sx={{
+					display: "flex",
+					width: "100%",
+					overflow: "auto",
+					paddingBottom: "15px",
+				}}
+			>
+				<SideDrawer />
+				<Box sx={{ width: "100%" }} mt={3}>
+					<Notes />
+				</Box>
+				<Profile
+					isOpen={accountMenuOptions.isProfileOpen}
+					setIsOpen={accountMenuOptions.setIsProfileOpen}
+				/>
+				<AddNoteModal open={isNoteModalOpen} setOpen={setIsNoteModalOpen} />
+				<AlertMessage
+					open={accountMenuOptions.isError}
+					setOpen={accountMenuOptions.setIsError}
+					message="Logout error, please try later."
+				/>
+			</Box>
+			<AddButton openAddTodoModal={setIsNoteModalOpen} />
+		</Fragment>
+	);
 }
 
 export default Home;
