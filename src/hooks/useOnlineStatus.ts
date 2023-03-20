660eca3414305ed01ddf5e43d9d871b297906ad3
Ashish <ashishcbscboard@gmail.com>
2023-03-20 12:07:48 +0530

    refactor: used capacitor network plugin for connectivity

--- src/hooks/useOnlineStatus.ts
+++ src/hooks/useOnlineStatus.ts
@@ -1,22 +1,21 @@
 import { useEffect, useState } from "react";
+import { ConnectionStatus, Network } from "@capacitor/network";
 
 function useOnlineStatus() {
-  const [isOnline, setIsOnline] = useState(window.navigator.onLine);
-  useEffect(() => {
-    function handleOnline() {
-      setIsOnline(true);
-    }
-    function handleOffline() {
-      setIsOnline(false);
-    }
-    window.addEventListener("online", handleOnline);
-    window.addEventListener("offline", handleOffline);
-    return () => {
-      window.removeEventListener("online", handleOnline);
-      window.removeEventListener("offline", handleOffline);
-    };
-  }, []);
-  return isOnline;
+	const [isOnline, setIsOnline] = useState(true);
+
+	useEffect(() => {
+		function onStatusChange(status: ConnectionStatus) {
+			setIsOnline(status.connected);
+		}
+
+		Network.addListener("networkStatusChange", onStatusChange);
+
+		return () => {
+			Network.addListener("networkStatusChange", onStatusChange);
+		};
+	}, []);
+	return isOnline;
 }
 
 export default useOnlineStatus;
