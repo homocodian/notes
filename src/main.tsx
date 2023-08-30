67cf0ebda0264bd55d66384497e79c9a00a4bf04
Ashish <ashishcbscboard@gmail.com>
2023-08-30 14:34:11 +0530

    chore: updated react & vite to latest

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import { BrowserRouter as Router } from "react-router-dom";
 
@@ -7,13 +7,15 @@ import "./index.css";
 import App from "./App";
 import AuthProvider from "./context/AuthContext";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+root.render(
+	<React.StrictMode>
+		<Router>
+			<AuthProvider>
+				<App />
+			</AuthProvider>
+		</Router>
+	</React.StrictMode>
 );
