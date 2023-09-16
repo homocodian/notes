22ec3bccf3cf83243a378aaf5a734e70d56785e8
Ashish <ashishcbscboard@gmail.com>
2023-09-16 11:36:50 +0530

    chore: added google auth package for native auth

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,16 +1,21 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.notes.homocodians",
-  appName: "Notes",
-  webDir: "dist",
-  bundledWebRuntime: false,
-  plugins: {
-    SplashScreen: {
-      launchAutoHide: true,
-      launchShowDuration: 0,
-    },
-  },
+	appId: "com.notes.homocodians",
+	appName: "Notes",
+	webDir: "dist",
+	bundledWebRuntime: false,
+	plugins: {
+		SplashScreen: {
+			launchAutoHide: true,
+			launchShowDuration: 0,
+		},
+		GoogleAuth: {
+			scopes: ["profile", "email"],
+			serverClientId: "xxxxxx-xxxxxxxxxxxxxxxxxx.apps.googleusercontent.com",
+			forceCodeForRefreshToken: true,
+		},
+	},
 };
 
 export default config;
