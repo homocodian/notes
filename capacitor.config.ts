8eff2a2f8b50113eef3061a5b645d691d213ee8c
Ashish <ashishcbscboard@gmail.com>
2023-11-05 19:12:38 +0530

    chore: updated capacitor to v5

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,22 +1,21 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-	appId: "com.notes.homocodians",
-	appName: "Notes",
-	webDir: "dist",
-	bundledWebRuntime: false,
-	plugins: {
-		SplashScreen: {
-			launchAutoHide: true,
-			launchShowDuration: 0,
-		},
-		GoogleAuth: {
-			scopes: ["profile", "email"],
-			serverClientId:
-				"754355741739-t7sec02ku3v1k0dt3opc4e2mktphq8m1.apps.googleusercontent.com",
-			forceCodeForRefreshToken: true,
-		},
-	},
+  appId: "com.notes.homocodians",
+  appName: "Notes",
+  webDir: "dist",
+  plugins: {
+    SplashScreen: {
+      launchAutoHide: true,
+      launchShowDuration: 0,
+    },
+    GoogleAuth: {
+      scopes: ["profile", "email"],
+      serverClientId:
+        "754355741739-t7sec02ku3v1k0dt3opc4e2mktphq8m1.apps.googleusercontent.com",
+      forceCodeForRefreshToken: true,
+    },
+  }
 };
 
 export default config;
