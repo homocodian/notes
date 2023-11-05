065bbcf8f82561b7be3dca2245229afe6addfd77
Ashish <ashishcbscboard@gmail.com>
2023-11-05 23:33:44 +0530

    refactor: sliding searchbar in appbar

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,68 +1,113 @@
-import { Fragment } from "react";
+import { Fragment, useMemo, useRef, useState } from "react";
 import { useLocation } from "react-router";
 
+import ClearIcon from "@mui/icons-material/Clear";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import MuiAppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
 import Slide from "@mui/material/Slide";
 import Toolbar from "@mui/material/Toolbar";
-import MuiAppBar from "@mui/material/AppBar";
-import MenuIcon from "@mui/icons-material/Menu";
 import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
 
 import { routeNames } from "@/Routes";
-import { useAuth } from "@/context/AuthContext";
 import Searchbar from "@/components/Searchbar";
-import { useDrawer } from "@/context/DrawerContext";
-import SettingsMenu from "@/components/general/SettingsMenu";
 import KeyboardShortcut from "@/components/general/KeyboardShortcut";
+import SettingsMenu from "@/components/general/SettingsMenu";
+import { useAuth } from "@/context/AuthContext";
+import { useDrawer } from "@/context/DrawerContext";
 
 function AppBar() {
-	const { user } = useAuth();
-	const location = useLocation();
-	const { isDrawerOpen, setDrawerIsOpen } = useDrawer();
+  const nodeRef = useRef();
+  const { user } = useAuth();
+  const location = useLocation();
+  const { isDrawerOpen, setDrawerIsOpen } = useDrawer();
+  const [shouldShowToolbar, setShouldShowToolbar] = useState(true);
+  const [shouldShowSearchbar, setShouldShowSearchbar] = useState(false);
 
-	const shouldShow = !!routeNames.find((item) => item === location.pathname);
+  const shouldShow = useMemo(() => {
+    return !!routeNames.find((item) => item === location.pathname);
+  }, [location.pathname]);
 
-	return (
-		<Fragment>
-			<Box sx={{ flexGrow: 1 }}>
-				<Slide direction="down" in={shouldShow}>
-					<MuiAppBar position="fixed">
-						<Toolbar
-							sx={{
-								justifyContent: "end",
-							}}
-						>
-							{user && (
-								<IconButton
-									size="large"
-									edge="start"
-									color="inherit"
-									aria-label="menu"
-									sx={{ mr: 2 }}
-									onClick={() => {
-										setDrawerIsOpen(!isDrawerOpen);
-									}}
-								>
-									<MenuIcon />
-								</IconButton>
-							)}
-							<Typography
-								component="div"
-								variant="h6"
-								sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-							>
-								Notes
-							</Typography>
-							{user?.uid ? <Searchbar /> : null}
-							<SettingsMenu />
-						</Toolbar>
-					</MuiAppBar>
-				</Slide>
-			</Box>
-			<KeyboardShortcut />
-		</Fragment>
-	);
+  return (
+    <Fragment>
+      <Slide direction="down" in={shouldShow}>
+        <MuiAppBar position="fixed">
+          <Box ref={nodeRef} overflow="hidden" flexGrow={1}>
+            <Toolbar>
+              <Slide
+                direction="down"
+                in={shouldShowToolbar}
+                mountOnEnter
+                unmountOnExit
+                container={nodeRef.current}
+                onExited={() => setShouldShowSearchbar(true)}
+              >
+                <Box
+                  flexGrow={1}
+                  display="flex"
+                  alignItems="center"
+                  position="relative"
+                >
+                  {user && (
+                    <IconButton
+                      size="large"
+                      edge="start"
+                      color="inherit"
+                      aria-label="menu"
+                      sx={{ mr: 2 }}
+                      onClick={() => {
+                        setDrawerIsOpen(!isDrawerOpen);
+                      }}
+                    >
+                      <MenuIcon />
+                    </IconButton>
+                  )}
+                  <Typography
+                    component="div"
+                    variant="h6"
+                    sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+                  >
+                    Notes
+                  </Typography>
+                  <IconButton onClick={() => setShouldShowToolbar(false)}>
+                    <SearchIcon htmlColor="#fff" />
+                  </IconButton>
+                  <SettingsMenu />
+                </Box>
+              </Slide>
+              <Slide
+                direction="up"
+                in={shouldShowSearchbar}
+                mountOnEnter
+                unmountOnExit
+                container={nodeRef.current}
+                onExited={() => {
+                  setShouldShowToolbar(true);
+                }}
+              >
+                <Box
+                  flexGrow={1}
+                  display="flex"
+                  alignItems="center"
+                  position="relative"
+                  justifyContent="space-between"
+                  gap="1rem"
+                >
+                  <Searchbar />
+                  <IconButton onClick={() => setShouldShowSearchbar(false)}>
+                    <ClearIcon htmlColor="#fff" />
+                  </IconButton>
+                </Box>
+              </Slide>
+            </Toolbar>
+          </Box>
+        </MuiAppBar>
+      </Slide>
+      <KeyboardShortcut />
+    </Fragment>
+  );
 }
 
 export default AppBar;
--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,85 +1,71 @@
 import { useSearchParams } from "react-router-dom";
 
-import { InputBase, alpha, styled } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import { InputBase, styled } from "@mui/material";
 
 const Search = styled("div")(({ theme }) => ({
-	position: "relative",
-	borderRadius: theme.shape.borderRadius,
-	backgroundColor: alpha(theme.palette.common.white, 0.15),
-	"&:hover": {
-		backgroundColor: alpha(theme.palette.common.white, 0.25),
-	},
-	marginLeft: 0,
-	width: "100%",
-	[theme.breakpoints.up("sm")]: {
-		marginLeft: theme.spacing(1),
-		width: "auto",
-	},
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  width: "100%",
+  flexGrow: 1,
+  display: "flex",
 }));
 
 const SearchIconWrapper = styled("div")(({ theme }) => ({
-	padding: theme.spacing(0, 2),
-	height: "100%",
-	position: "absolute",
-	pointerEvents: "none",
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "center",
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-	color: "inherit",
-	"& .MuiInputBase-input": {
-		padding: theme.spacing(1, 1, 1, 0),
-		// vertical padding + font size from searchIcon
-		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-		transition: theme.transitions.create("width"),
-		width: "100%",
-		[theme.breakpoints.up("sm")]: {
-			width: "12ch",
-			"&:focus": {
-				width: "20ch",
-			},
-		},
-	},
+  color: "inherit",
+  flexGrow: 1,
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+  },
 }));
 
-const Form = styled("form")(({ theme }) => ({
-	[theme.breakpoints.down("sm")]: {
-		flexGrow: 1,
-	},
-}));
+const Form = styled("form")`
+  flex-grow: 1;
+`;
 
 function Searchbar() {
-	const [searchParams, setSearchParams] = useSearchParams();
-	const query = searchParams.get("q") ?? "";
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get("q") ?? "";
 
-	return (
-		<Form
-			onSubmit={(e) => {
-				e.preventDefault();
-				const form = new FormData(e.currentTarget);
-				const search = form.get("search")?.toString() ?? "";
-				searchParams.set("q", search);
-				setSearchParams(searchParams);
-			}}
-		>
-			<Search>
-				<SearchIconWrapper>
-					<SearchIcon />
-				</SearchIconWrapper>
-				<StyledInputBase
-					placeholder="Search…"
-					inputProps={{ "aria-label": "search" }}
-					defaultValue={query}
-					key={query}
-					name="search"
-					autoComplete="off"
-				/>
-			</Search>
-		</Form>
-	);
+  return (
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const form = new FormData(e.currentTarget);
+        const search = form.get("search")?.toString() ?? "";
+        searchParams.set("q", search);
+        setSearchParams(searchParams);
+      }}
+    >
+      <Search>
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          placeholder="Search…"
+          inputProps={{ "aria-label": "search" }}
+          defaultValue={query}
+          key={query}
+          name="search"
+          autoComplete="off"
+        />
+      </Search>
+    </Form>
+  );
 }
 
 export default Searchbar;
