5f35775373559459328f87a7477eb473d43e99fc
Ashish <ashishcbscboard@gmail.com>
2023-11-06 20:15:21 +0530

    fix: statusbar is not available in web

--- src/utils/change-statusbar-color.ts
+++ src/utils/change-statusbar-color.ts
@@ -2,20 +2,22 @@ import { Capacitor } from "@capacitor/core";
 import { StatusBar, Style } from "@capacitor/status-bar";
 
 export async function changeStatusbarColor(isDarkMode: boolean) {
-	if (Capacitor.getPlatform() === "android") {
-		if (isDarkMode) {
-			await StatusBar.setBackgroundColor({ color: "#272727" });
-		} else {
-			await StatusBar.setBackgroundColor({ color: "#ff5722" });
-		}
-		await StatusBar.setStyle({ style: Style.Dark });
-	}
+  if (Capacitor.isNativePlatform()) {
+    if (isDarkMode) {
+      await StatusBar.setBackgroundColor({ color: "#272727" });
+    } else {
+      await StatusBar.setBackgroundColor({ color: "#ff5722" });
+    }
+    await StatusBar.setStyle({ style: Style.Dark });
+  }
 }
 
 export async function setStatusbarColor(color: string, style?: Style) {
-	await StatusBar.setBackgroundColor({ color });
+  if (!Capacitor.isNativePlatform()) return;
 
-	if (style) {
-		await StatusBar.setStyle({ style });
-	}
+  await StatusBar.setBackgroundColor({ color });
+
+  if (style) {
+    await StatusBar.setStyle({ style });
+  }
 }
