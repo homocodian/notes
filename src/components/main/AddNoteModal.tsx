977a4d7d4fe9583aa16644e96bf72833d98c4f87
Ashish <ashishcbscboard@gmail.com>
2023-11-06 20:18:02 +0530

    refactor: shows error when no network is available while adding note

--- src/components/main/AddNoteModal.tsx
+++ src/components/main/AddNoteModal.tsx
@@ -1,138 +1,138 @@
-import { useEffect, useRef, useState } from "react";
-import {
-	useMediaQuery,
-	Dialog,
-	DialogContent,
-	DialogContentText,
-	DialogActions,
-	Button,
-	TextField,
-	Chip,
-	MenuItem,
-} from "@mui/material";
 import { LoadingButton } from "@mui/lab";
+import {
+  Button,
+  Chip,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  MenuItem,
+  TextField,
+  useMediaQuery,
+} from "@mui/material";
 import Select from "@mui/material/Select";
 import { useTheme } from "@mui/material/styles";
+import { useEffect, useRef, useState } from "react";
 import { useHotkeys } from "react-hotkeys-hook";
 
 import { useAddNote } from "@/hooks";
 
 interface IProps {
-	open: boolean;
-	setOpen: (value: boolean) => void;
+  open: boolean;
+  setOpen: (value: boolean) => void;
 }
 
 function AddNoteModal({ open, setOpen }: IProps) {
-	const theme = useTheme();
-	const [addNote, loading] = useAddNote();
-	const [noteError, setNoteError] = useState(false);
-	const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
-	const formRef = useRef<HTMLFormElement | null>(null);
-	const inputRef = useRef<HTMLTextAreaElement | null>(null);
-	const submitButtonRef = useRef<HTMLButtonElement | null>(null);
+  const theme = useTheme();
+  const [addNote, loading] = useAddNote();
+  const [noteError, setNoteError] = useState(false);
+  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const formRef = useRef<HTMLFormElement | null>(null);
+  const inputRef = useRef<HTMLTextAreaElement | null>(null);
+  const submitButtonRef = useRef<HTMLButtonElement | null>(null);
 
-	useHotkeys(
-		"shift+enter",
-		(e) => {
-			e.preventDefault();
-			submitButtonRef.current?.click();
-		},
-		{
-			enableOnFormTags: ["INPUT", "TEXTAREA"],
-		}
-	);
+  useHotkeys(
+    "shift+enter",
+    (e) => {
+      e.preventDefault();
+      submitButtonRef.current?.click();
+    },
+    {
+      enableOnFormTags: ["INPUT", "TEXTAREA"],
+    },
+  );
 
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			inputRef.current?.focus();
-		}, 0);
-		return () => clearTimeout(timer);
-	}, [open]);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      inputRef.current?.focus();
+    }, 0);
+    return () => clearTimeout(timer);
+  }, [open]);
 
-	const handleClose = () => {
-		setNoteError(false);
-		setOpen(false);
-	};
+  const handleClose = () => {
+    setNoteError(false);
+    setOpen(false);
+  };
 
-	const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		const formData = new FormData(e.currentTarget);
-		const note = formData.get("note")?.toString();
-		const category = formData.get("category")?.toString()!;
-		if (note === "" || !note) {
-			setNoteError(true);
-			return;
-		}
-		await addNote(note, category);
-		formRef.current?.reset();
-		handleClose();
-	};
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const note = formData.get("note")?.toString();
+    const category = formData.get("category")?.toString()!;
+    if (note === "" || !note) {
+      setNoteError(true);
+      return;
+    }
+    await addNote(note, category);
+    formRef.current?.reset();
+    handleClose();
+  };
 
-	return (
-		<Dialog
-			open={open}
-			aria-labelledby="add note"
-			fullScreen={fullScreen}
-			fullWidth
-			closeAfterTransition
-			onKeyDown={(e) => {
-				if (e.key === "Escape") {
-					setOpen(false);
-				}
-			}}
-		>
-			<DialogContent>
-				<DialogContentText>Note</DialogContentText>
-			</DialogContent>
+  return (
+    <Dialog
+      open={open}
+      aria-labelledby="add note"
+      fullScreen={fullScreen}
+      fullWidth
+      closeAfterTransition
+      onKeyDown={(e) => {
+        if (e.key === "Escape") {
+          setOpen(false);
+        }
+      }}
+    >
+      <DialogContent>
+        <DialogContentText>Note</DialogContentText>
+      </DialogContent>
 
-			<DialogContent>
-				<form onSubmit={onSubmit} id="note_form" ref={formRef}>
-					<TextField
-						id="todo"
-						error={noteError}
-						multiline
-						minRows={4}
-						fullWidth
-						name="note"
-						label="Note"
-						sx={{ marginBottom: "1rem" }}
-						required
-						inputRef={inputRef}
-					/>
-					<Select
-						sx={{ marginTop: "1rem" }}
-						id="category"
-						fullWidth
-						name="category"
-						defaultValue="general"
-						inputProps={{ "aria-label": "select category" }}
-						renderValue={(value) => (
-							<Chip label={value} sx={{ textTransform: "capitalize" }} />
-						)}
-					>
-						<MenuItem value={"general"}>General</MenuItem>
-						<MenuItem value={"important"}>Important</MenuItem>
-					</Select>
-				</form>
-			</DialogContent>
+      <DialogContent>
+        <form onSubmit={onSubmit} id="note_form" ref={formRef}>
+          <TextField
+            id="todo"
+            error={noteError}
+            multiline
+            minRows={4}
+            fullWidth
+            name="note"
+            label="Note"
+            sx={{ marginBottom: "1rem" }}
+            required
+            inputRef={inputRef}
+          />
+          <Select
+            sx={{ marginTop: "1rem" }}
+            id="category"
+            fullWidth
+            name="category"
+            defaultValue="general"
+            inputProps={{ "aria-label": "select category" }}
+            renderValue={(value) => (
+              <Chip label={value} sx={{ textTransform: "capitalize" }} />
+            )}
+          >
+            <MenuItem value={"general"}>General</MenuItem>
+            <MenuItem value={"important"}>Important</MenuItem>
+          </Select>
+        </form>
+      </DialogContent>
 
-			<DialogActions>
-				<Button variant="text" onClick={handleClose}>
-					Cancel
-				</Button>
-				<LoadingButton
-					type="submit"
-					form="note_form"
-					variant="contained"
-					loading={loading}
-					disableElevation
-					ref={submitButtonRef}
-				>
-					Submit
-				</LoadingButton>
-			</DialogActions>
-		</Dialog>
-	);
+      <DialogActions>
+        <Button variant="text" onClick={handleClose}>
+          Cancel
+        </Button>
+        <LoadingButton
+          type="submit"
+          form="note_form"
+          variant="contained"
+          loading={loading}
+          disableElevation
+          ref={submitButtonRef}
+        >
+          Submit
+        </LoadingButton>
+      </DialogActions>
+    </Dialog>
+  );
 }
 
 export default AddNoteModal;
--- src/hooks/useAddNote.ts
+++ src/hooks/useAddNote.ts
@@ -2,55 +2,60 @@ import { useCallback, useState } from "react";
 
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
-import { db } from "../firebase";
-import useOnlineStatus from "@/hooks/useOnlineStatus";
 import { useAuth } from "@/context/AuthContext";
+import { db } from "@/firebase";
+import useOnlineStatus from "@/hooks/useOnlineStatus";
 import { User } from "firebase/auth";
+import toast from "react-hot-toast";
 
 type UseAddNote = [
-	addNote: (text: string, category: string) => Promise<void>,
-	loading: boolean,
-	error: string | null
+  addNote: (text: string, category: string) => Promise<void>,
+  loading: boolean,
+  error: string | null,
 ];
 
 function useAddNote(): UseAddNote {
-	const { user } = useAuth();
-	const isOnline = useOnlineStatus();
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState<string | null>(null);
+  const { user } = useAuth();
+  const isOnline = useOnlineStatus();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
-	const addNote = useCallback(async (text: string, category: string) => {
-		if (!isOnline || !user) {
-			setError("Check your internet.");
-			return;
-		}
-		setLoading(true);
-		try {
-			const data = formatNote(user, text, category);
-			await addDoc(collection(db, "notes"), data);
-			setError(null);
-		} catch (error) {
-			setError("Adding note failed.");
-		} finally {
-			setLoading(false);
-		}
-	}, []);
+  const addNote = useCallback(
+    async (text: string, category: string) => {
+      if (!isOnline || !user) {
+        toast.error("Check your internet.");
+        setError("Check your internet.");
+        return;
+      }
+      setLoading(true);
+      try {
+        const data = formatNote(user, text, category);
+        await addDoc(collection(db, "notes"), data);
+        setError(null);
+      } catch (error) {
+        setError("Adding note failed.");
+      } finally {
+        setLoading(false);
+      }
+    },
+    [isOnline, user],
+  );
 
-	return [addNote, loading, error];
+  return [addNote, loading, error];
 }
 
 export default useAddNote;
 
 function formatNote(user: User, text: string, category: string) {
-	return {
-		text,
-		category,
-		userId: user.uid,
-		name: user.displayName,
-		email: user.email,
-		timestamp: serverTimestamp(),
-		updatedAt: serverTimestamp(),
-		...(user?.displayName ? { name: user.displayName } : {}),
-		...(user?.email ? { email: user.email } : {}),
-	};
+  return {
+    text,
+    category,
+    userId: user.uid,
+    name: user.displayName,
+    email: user.email,
+    timestamp: serverTimestamp(),
+    updatedAt: serverTimestamp(),
+    ...(user?.displayName ? { name: user.displayName } : {}),
+    ...(user?.email ? { email: user.email } : {}),
+  };
 }
