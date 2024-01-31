7fa6e3007998119dda9991a8667f22ac7a0d527a
Ashish <homocodian@gmail.com>
2024-01-31 22:05:32 +0530

    chore: serverless functions setup

---
+++ functions/hello.ts
@@ -0,0 +1,59 @@
+import { type Handler } from "@netlify/functions";
+
+type Success = {
+  success: true;
+  data: object;
+};
+
+type Error = {
+  success: false;
+  error: string;
+};
+
+type ResponseBody = Success | Error;
+
+export const handler: Handler = async (event) => {
+  if (event.httpMethod === "GET") {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: true,
+        data: {
+          message: "Hello!",
+        },
+      } satisfies ResponseBody),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Authorization, Content-Type",
+        "Content-Type": "application/json",
+      },
+    };
+  }
+
+  if (!event.body) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        success: false,
+        error: "Please provide valid field",
+      } satisfies ResponseBody),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Authorization, Content-Type",
+        "Content-Type": "application/json",
+      },
+    };
+  }
+
+  let data = JSON.parse(event.body);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ success: true, data } satisfies ResponseBody),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Authorization, Content-Type",
+      "Content-Type": "application/json",
+    },
+  };
+};
