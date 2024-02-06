5b2bc3bcc3b7fcac7565c242829584267b966580
Ashish <homocodian@gmail.com>
2024-02-06 23:00:05 +0530

    refactor: removed client firestore

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,11 +5,6 @@ import {
   indexedDBLocalPersistence,
   initializeAuth,
 } from "firebase/auth";
-import {
-  connectFirestoreEmulator,
-  doc,
-  getFirestore,
-} from "firebase/firestore";
 
 import { Capacitor } from "@capacitor/core";
 import { firebaseConfig } from "./config/firebase.config";
@@ -44,23 +39,5 @@ function whichAuth() {
   return auth;
 }
 
-function getFirestoreDB() {
-  if (
-    import.meta.env.DEV &&
-    import.meta.env.VITE_APP_ENV &&
-    import.meta.env.VITE_APP_ENV === "emulator"
-  ) {
-    const emulatedDB = getFirestore();
-    connectFirestoreEmulator(emulatedDB, "127.0.0.1", 8080);
-    return emulatedDB;
-  }
-
-  const db = getFirestore(app);
-  return db;
-}
-
 export default app;
 export const auth = whichAuth();
-export const db = getFirestoreDB();
-export const noteDocReference = (id: string) => doc(db, "notes", id);
-export const noteReference = () => doc(db, "notes");
