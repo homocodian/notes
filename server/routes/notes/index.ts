1229eee59390972202c50c6a9e4f3c64f374ad86
homocodian <homocodian@gmail.com>
2024-05-01 02:12:37 +0530

    chore: removed serverless backend

--- server/routes/notes/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as DELETE } from "./delete";
-export { default as GET } from "./get";
-export { default as PATCH } from "./patch";
-export { default as POST } from "./post";
--- server/utils/is-firebase-error.ts
+++
@@ -1,5 +0,0 @@
-import { FirebaseError } from "firebase-admin";
-
-export function isFirebaseError(error: unknown): error is FirebaseError {
-  return (error as FirebaseError).code !== undefined;
-}
