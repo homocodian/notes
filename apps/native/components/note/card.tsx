db925646bf691cd8fcb56a4e20f318f823cbea91
homocodian <homocodian@gmail.com>
2024-05-23 21:55:27 +0530

    feat: copy note

--- apps/native/components/note/card.tsx
+++ apps/native/components/note/card.tsx
@@ -1,49 +1,52 @@
-import { Note } from "@/types/note";
 import React from "react";
 import { View } from "react-native";
 import { Card, Chip, Text, useTheme } from "react-native-paper";
+
+import { Note } from "@/types/note";
+
 import { Menu } from "./menu";
 
 interface NoteCardProps extends Note {}
 
 export function NoteCard({
-	category,
-	text,
-	createdAt,
-	isComplete,
+  id,
+  category,
+  text,
+  createdAt,
+  isComplete
 }: NoteCardProps) {
-	const theme = useTheme();
+  const theme = useTheme();
 
-	return (
-		<Card>
-			<Card.Content className="space-y-4 pt-1">
-				<View className="flex flex-row justify-between items-center">
-					<View>
-						<Chip
-							textStyle={{
-								textTransform: "capitalize",
-							}}
-						>
-							{category}
-						</Chip>
-					</View>
-					<View>
-						<Menu />
-					</View>
-				</View>
-				<Text
-					style={{
-						textDecorationLine: isComplete ? "line-through" : undefined,
-					}}
-				>
-					{text}
-				</Text>
-				<View>
-					<Text style={{ color: theme.colors.onSurfaceDisabled }}>
-						Date created {new Date(createdAt).toLocaleDateString()}
-					</Text>
-				</View>
-			</Card.Content>
-		</Card>
-	);
+  return (
+    <Card>
+      <Card.Content className="space-y-4 pt-1">
+        <View className="flex flex-row justify-between items-center">
+          <View>
+            <Chip
+              textStyle={{
+                textTransform: "capitalize"
+              }}
+            >
+              {category}
+            </Chip>
+          </View>
+          <View>
+            <Menu id={id} text={text} />
+          </View>
+        </View>
+        <Text
+          style={{
+            textDecorationLine: isComplete ? "line-through" : undefined
+          }}
+        >
+          {text}
+        </Text>
+        <View>
+          <Text style={{ color: theme.colors.onSurfaceDisabled }}>
+            Date created {new Date(createdAt).toLocaleDateString()}
+          </Text>
+        </View>
+      </Card.Content>
+    </Card>
+  );
 }
--- apps/native/components/note/menu.tsx
+++ apps/native/components/note/menu.tsx
@@ -1,28 +1,47 @@
 import React from "react";
 import { IconButton, Menu as PaperMenu } from "react-native-paper";
 
-export function Menu() {
-	const [visible, setVisible] = React.useState(false);
+import { copyString } from "@/lib/copy";
+import { toast } from "@/lib/toast";
 
-	const openMenu = () => setVisible(true);
+type MenuProps = {
+  id: number;
+  text: string;
+};
 
-	const closeMenu = () => setVisible(false);
-	return (
-		<PaperMenu
-			visible={visible}
-			onDismiss={closeMenu}
-			anchor={<IconButton onPress={openMenu} icon="dots-vertical" />}
-			anchorPosition="bottom"
-		>
-			<PaperMenu.Item leadingIcon="check" onPress={() => {}} title="Done" />
-			<PaperMenu.Item leadingIcon="delete" onPress={() => {}} title="Delete" />
-			<PaperMenu.Item leadingIcon="pencil" onPress={() => {}} title="Edit" />
-			<PaperMenu.Item
-				leadingIcon="content-copy"
-				onPress={() => {}}
-				title="Copy"
-			/>
-			<PaperMenu.Item leadingIcon="share" onPress={() => {}} title="Share" />
-		</PaperMenu>
-	);
+export function Menu({ id, text }: MenuProps) {
+  const [visible, setVisible] = React.useState(false);
+
+  const openMenu = () => setVisible(true);
+
+  const closeMenu = () => setVisible(false);
+
+  const copyText = async () => {
+    const copied = await copyString(text);
+    if (!copied) toast("Failed to copy.");
+  };
+
+  const handleClick = (cb: () => void) => () => {
+    closeMenu();
+    cb();
+  };
+
+  return (
+    <PaperMenu
+      visible={visible}
+      onDismiss={closeMenu}
+      anchor={<IconButton onPress={openMenu} icon="dots-vertical" />}
+      anchorPosition="bottom"
+    >
+      <PaperMenu.Item leadingIcon="check" onPress={() => {}} title="Done" />
+      <PaperMenu.Item leadingIcon="delete" onPress={() => {}} title="Delete" />
+      <PaperMenu.Item leadingIcon="pencil" onPress={() => {}} title="Edit" />
+      <PaperMenu.Item
+        leadingIcon="content-copy"
+        onPress={handleClick(copyText)}
+        title="Copy"
+      />
+      <PaperMenu.Item leadingIcon="share" onPress={() => {}} title="Share" />
+    </PaperMenu>
+  );
 }
